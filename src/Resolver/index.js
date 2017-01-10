d419a1f1320a46d874debc93415f1ce7361f7bd2
Romain Lanz <romain.lanz@slynova.ch>
2017-01-10 10:35:21 +0100

    doc(docblock): use YUIDoc syntax

--- src/Resolver/index.js
+++ src/Resolver/index.js
@@ -8,12 +8,35 @@
 
 const NE = require('node-exceptions')
 
+/**
+ * Validate, resolve and execute bindings attached as a closure or IoC Container binding.
+ *
+ * @class Resolver
+ * @module Adonis
+ * @submodule binding-resolver
+ */
 class Resolver {
 
+  /**
+   * Constructor.
+   *
+   * @constructor
+   * @param  {object} Ioc
+   * @return {void}
+   */
   constructor (Ioc) {
     this.Ioc = Ioc
   }
 
+  /**
+   * Validates a binding to be sure that it must points to a valid
+   * namespace or closure and IoC Container binding points to a method.
+   *
+   * @method validateBinding
+   * @param  {function|string} binding
+   * @throws {InvalidArgumentException} E_INVALID_IOC_BINDING
+   * @return {void}
+   */
   validateBinding (binding) {
     if (typeof (binding) !== 'function' && typeof (binding) !== 'string') {
       throw new NE.InvalidArgumentException('Handler must point to a valid namespace or a closure', 500, 'E_INVALID_IOC_BINDING')
@@ -23,6 +46,13 @@ class Resolver {
     }
   }
 
+  /**
+   * Resolves a binding depending of its type.
+   *
+   * @method resolveBinding
+   * @param  {function|string} binding
+   * @return {mixed}
+   */
   resolveBinding (binding) {
     if (typeof (binding) === 'function') {
       return binding
@@ -32,6 +62,15 @@ class Resolver {
     }
   }
 
+  /**
+   * Executes a binding depending of its type.
+   *
+   * @method executeBinding
+   * @param  {function|string} binding
+   * @param  {array} args
+   * @param  {object} customInstance
+   * @return {mixed}
+   */
   executeBinding (binding, args, customInstance) {
     args = args || []
     const resolvedBinding = this.resolveBinding(binding)
